d913d4989b4d50004f33d5d599a74848e977fe84
bruceleejr <theyukidjr@gmail.com>
2023-06-01 22:37:13 -0300

    reworked JSONData class to work with list of json

--- lib/json_data.dart
+++ lib/json_data.dart
@@ -1,20 +1,30 @@
 import 'imports.dart';
 
 class JSONData implements Data {
-  Map<String, dynamic> _data = {};
+  List<Map<String, dynamic>> _data = [];
 
   @override
   void load(String fileName) {
-    final file = File(fileName);
-    final jsonString = file.readAsStringSync();
-    _data = json.decode(jsonString);
+    try {
+      final file = File(fileName);
+      final jsonString = file.readAsStringSync();
+      final dataList = json.decode(jsonString) as List<dynamic>;
+
+      _data = dataList.cast<Map<String, dynamic>>();
+    } catch (e) {
+      throw InvalidDataFormatException('Invalid JSON data format: $e');
+    }
   }
 
   @override
   void save(String fileName) {
-    final file = File(fileName);
-    final jsonString = json.encode(_data);
-    file.writeAsStringSync(jsonString);
+    try {
+      final file = File(fileName);
+      final jsonString = json.encode(_data);
+      file.writeAsStringSync(jsonString);
+    } catch (e) {
+      throw InvalidDataFormatException('Error saving JSON data: $e');
+    }
   }
 
   @override
@@ -31,16 +41,17 @@ class JSONData implements Data {
   @override
   set data(String data) {
     try {
-      _data = json.decode(data);
+      final dataList = json.decode(data) as List<dynamic>;
+      _data = dataList.cast<Map<String, dynamic>>();
     } catch (e) {
-      throw InvalidDataFormatException('Invalid data format');
+      throw InvalidDataFormatException('Invalid JSON data format: $e');
     }
   }
 
   @override
   List<String> get fields {
     if (_data.isNotEmpty) {
-      return _data.keys.toList();
+      return _data.first.keys.toList();
     }
     return [];
   }
